33ef213539a63f132e9c9a409c26c996631bb560
Don Hardman <don.hardman@muvon.io>
2021-07-14 21:03:50 +0700

    Add webpack addon

--- app/main.php
+++ app/main.php
@@ -8,5 +8,7 @@ $View = App::process()
   ->append('_foot')
 ;
 
+$View->assign('BUNDLE_HASH', filemtime(getenv('VAR_DIR') . '/bundle.css'));
+
 Response::current()->send((string) $View->render());
 App::stop();
