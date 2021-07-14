6e593abeca0faf5e2459fb758de17379b80ee664
Don Hardman <don.hardman@muvon.io>
2021-07-14 21:06:19 +0700

    Install ardb

--- docker/env.sh
+++ docker/env.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 # Any environment variables here
 
+
+STORAGE_HOST=127.0.0.1
+STORAGE_PORT=2112
+
+export STORAGE_HOST \
+  STORAGE_PORT \
+  STORAGE_BIND="${STORAGE_HOST}:${STORAGE_PORT}"
+
+
